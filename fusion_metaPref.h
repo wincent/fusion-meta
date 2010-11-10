50db32f6c57e7eb022f98db435dc9faf9cb3429c
Wincent Colaiuta <win@wincent.com>
2010-11-10 17:05:17 +0100

    New preference pane project (from template)
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ fusion_metaPref.h
@@ -0,0 +1,19 @@
+//
+//  fusion_metaPref.h
+//  fusion-meta
+//
+//  Created by Wincent Colaiuta on 10/11/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface fusion_metaPref : NSPreferencePane 
+{
+
+}
+
+- (void) mainViewDidLoad;
+
+@end
---
+++ fusion_metaPref.m
@@ -0,0 +1,18 @@
+//
+//  fusion_metaPref.m
+//  fusion-meta
+//
+//  Created by Wincent Colaiuta on 10/11/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "fusion_metaPref.h"
+
+
+@implementation fusion_metaPref
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
